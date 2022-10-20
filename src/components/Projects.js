6ff7d9607599a2720b3071e7bd26cb8c40444b48
Garret Gallant <gallanttechnological@gmail.com>
2022-10-20 17:01:00 -0600

    added full template onto first slide in carousel for each project, need to identify and resolve autoscrolling issue in carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,9 +12,15 @@ const Projects = () => {
         <br />
         <p className='font-bold'>The first Program I ever wrote to the DOM, uses the funtranslations Yodish API</p>
         <div className='flex gap-10 justify-center mt-10'>
-          <img src='./bm1.png' className='w-1/4'/>
-          <img src='./bm3.png' className='w-1/4'/>
-          <img src='./bm4.png' className='w-1/4'/>
+          <img src='./yt1.png' className='w-1/4'/>
+          <img src='./yt2.png' className='w-1/4'/>
+          <img src='./yt3.png' className='w-1/4'/>
+          <p class='fixed w-1/3 text-center top-1/3 mt-20'>
+            The site is minimal, however the fact that it was my first ever application that got rendered to a page and everything functioned as intended was gratifying,
+            I was so thrilled when we got it up, and while I struggled to get a few of the more simplistic features built, I learned so much about how
+            code is written, and how different functions make things happen within applications. It was also a pleasure to work with a strong team of
+            fellow learners so early into my software career.
+          </p>
           <a className='fixed w-40 bottom-10'href='https://yoda-text.netlify.app/' target='_blank'>
             <Button color='dark'>Live Demo</Button>
           </a>
@@ -28,13 +34,13 @@ const Projects = () => {
         <br />
         <p className='font-bold'>A project focused around cataloguing music and expanding musical preferences, using React and JSON</p>
         <div className='flex gap-10 justify-center mt-10'>
-          <img src='./bm1.png' className='w-1/4'/>
-          <img src='./bm3.png' className='w-1/4'/>
-          <img src='./bm4.png' className='w-1/4'/>
-          <a className='fixed w-40 bottom-10'href='https://www.loom.com/share/00b29a32fdcb43439f10006f9e49576a' target='_blank'>
-            <Button color='dark'>Video Demo</Button>
+          <img src='./hm1.png' className='w-1/4'/>
+          <img src='./hm2.png' className='w-1/4'/>
+          <img src='./hm3.png' className='w-1/4'/>
+          <a className='fixed w-40 bottom-10'href='https://haku-mele.netlify.app/random-song' target='_blank'>
+            <Button color='dark'>Live Demo</Button>
           </a>
-          <a className='fixed w-40 bottom-20 m-5' href='https://github.com/Garret-Gallant/bandmate-mono-repo' target='_blank'>
+          <a className='fixed w-40 bottom-20 m-5' href='https://github.com/Garret-Gallant/haku-mele-react-app' target='_blank'>
             <Button color='dark'>Github Source Code</Button>
           </a>
         </div>
@@ -44,14 +50,14 @@ const Projects = () => {
         <br />
         <p className='font-bold'>A project built in a week to emulate Hearthstone basic gameplay, using React and Rails</p>
         <div className='flex gap-10 justify-center mt-10'>
-          <img src='./bm1.png' className='w-1/4'/>
-          <img src='./bm3.png' className='w-1/4'/>
-          <img src='./bm4.png' className='w-1/4'/>
-          <a className='fixed w-40 bottom-10'href='https://www.loom.com/share/00b29a32fdcb43439f10006f9e49576a' target='_blank'>
-            <Button color='dark'>Video Demo</Button>
+          <img src='./cd1.png' className='w-1/4'/>
+          <img src='./cd2.png' className='w-1/4'/>
+          <img src='./cd3.png' className='w-1/4'/>
+          <a className='fixed w-40 bottom-20' href='https://github.com/Garret-Gallant/Phase-3-Project-Backend' target='_blank'>
+            <Button color='dark'>Backend Source Code</Button>
           </a>
-          <a className='fixed w-40 bottom-20 m-5' href='https://github.com/Garret-Gallant/bandmate-mono-repo' target='_blank'>
-            <Button color='dark'>Github Source Code</Button>
+          <a className='fixed w-40 bottom-40' href='https://github.com/Garret-Gallant/Phase-3-Project-Backend' target='_blank'>
+            <Button color='dark'>Frontend Source Code</Button>
           </a>
         </div>
       </div>
@@ -60,13 +66,10 @@ const Projects = () => {
         <br />
         <p className='font-bold'>A forum which utilizes a Rails custom backend and React frontend to host user accounts and catalogue user posts</p>
         <div className='flex gap-10 justify-center mt-10'>
-          <img src='./bm1.png' className='w-1/4'/>
-          <img src='./bm3.png' className='w-1/4'/>
-          <img src='./bm4.png' className='w-1/4'/>
-          <a className='fixed w-40 bottom-10'href='https://www.loom.com/share/00b29a32fdcb43439f10006f9e49576a' target='_blank'>
-            <Button color='dark'>Video Demo</Button>
-          </a>
-          <a className='fixed w-40 bottom-20 m-5' href='https://github.com/Garret-Gallant/bandmate-mono-repo' target='_blank'>
+          <img src='./so1.png' className='w-1/4'/>
+          <img src='./so2.png' className='w-1/4'/>
+          <img src='./so3.png' className='w-1/4'/>
+          <a className='fixed w-40 bottom-20 m-5' href='https://github.com/Garret-Gallant/stackoverflat-mono-repo' target='_blank'>
             <Button color='dark'>Github Source Code</Button>
           </a>
         </div>
