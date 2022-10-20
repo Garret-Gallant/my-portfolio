063b308730a5424de96214c935bab4ad6a1e3e8d
Garret Gallant <gallanttechnological@gmail.com>
2022-10-20 15:12:23 -0600

    added more refined homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="text-center bg-[url('./assets/background.gif')] h-screen text-white">
-      <h4>Garret Gallant's Portfolio Landing Page</h4>
+    <div className="text-center bg-[url('./assets/background.gif')] h-screen text-white flex flex-col items-center pt-10">
+      <h4 className='relative text-2xl font-bold'>Welcome to Garret Gallant's Portfolio</h4>
+      <img src='home.jpg' className=" relative h-1/3 mt-10 border-double border-8 border-slate-500 rounded-full"/>
+      <p className='font-bold text-xl mt-10'>Garret is a Full-Stack Developer from Denver, CO with a passion for innovation.</p>
+      <img src='./techs.png' className='mt-10 w-2/3'/>
     </div>
   )
 }
