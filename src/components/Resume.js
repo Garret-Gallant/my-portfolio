de5e283f4d6f1bce1f16ecb6072cc6761cba7650
Garret Gallant <gallanttechnological@gmail.com>
2022-10-20 11:43:08 -0600

    modified resume load-in

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import ResumeFile from './ResumeFile.pdf'
 
 const Resume = () => {
   return (
-    <div className="flex h-full items-center justify-center bg-[url('./assets/background.gif')] pt-5">
+    <div className="flex h-full items-center justify-center bg-[url('./assets/background.gif')] pt-20">
       <Document file={ResumeFile}>
         <Page pageNumber={1} onRenderAnnotationLayerSuccess={console.log} />
       </Document>
