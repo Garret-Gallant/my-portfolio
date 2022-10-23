964432729abde19d212e7fd3ec7d1799e6404186
Garret Gallant <gallanttechnological@gmail.com>
2022-10-23 14:30:14 -0600

    fixed resume download button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,20 @@
 import React from "react";
 
+
 const Resume = () => {
   const preview =
     "https://docs.google.com/document/d/1d8APwQA_0c4-pCgFHTezDVkAm9nv20U7qHcsKmZxfJE/preview";
 
   return (
     <div className="flex flex-col items-center justify-center bg-[url('./assets/background.gif')] p-20">
+      <a href="https://docs.google.com/document/d/1d8APwQA_0c4-pCgFHTezDVkAm9nv20U7qHcsKmZxfJE/export?format=pdf">
       <button
         className="fixed top-40 left-20 w-40 mb-10 border-2 border-slate-400 rounded-lg text-white p-2 bg-slate-800"
-        href="https://docs.google.com/document/d/1d8APwQA_0c4-pCgFHTezDVkAm9nv20U7qHcsKmZxfJE/export?format=pdf"
         color="dark"
       >
         Download A Copy
       </button>
+      </a>
       <iframe
         title="resume"
         src={preview}
