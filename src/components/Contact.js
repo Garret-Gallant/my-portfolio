36f4e81dae87a2ea76418896e6f9058026e6b13e
Garret Gallant <gallanttechnological@gmail.com>
2022-10-23 13:02:13 -0600

    further carousel styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,24 +8,31 @@ const Contact = () => {
 
   return (
     <div className="h-screen bg-[url('./assets/background.gif')]">
-      <form className=" flex flex-col items-center p-20">
+      <form action="mailto:gallanttechnological@gmail.com" method="get" enctype="text/plain" className=" flex flex-col items-center p-20">
+        <div>
         <input
           type="text"
           className="rounded-lg bg-slate-700 text-white placeholder-white"
           placeholder="Company/Name..."
         ></input>
+        <input
+          type="text"
+          className="rounded-lg bg-slate-700 text-white placeholder-white ml-5"
+          placeholder="Email..."
+        ></input>
+        </div>
         <textarea
           id="message"
           rows="6"
           className="rounded-lg w-full text-small mt-10 bg-slate-700 placeholder-white"
           placeholder="Message..."
         ></textarea>
-        <button
+        <input
+          type='submit'
+          value="Send Message"
           className="w-40 m-10 border-2 border-slate-400 rounded-lg text-white p-2 bg-slate-800 duration-300 hover:scale-110 hover:border-slate-900"
           onCLick={handleSubmit}
-        >
-          Send Message
-        </button>
+        ></input>
       </form>
     </div>
   );
